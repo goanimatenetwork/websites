b2faaaf1bd25fb354e2c5af515fe14a9e283fe0d
goanimatenetwork <goanimatenetwork@zohomail.com>
2022-09-23 12:17:11 -0700

    Add files via upload

---
+++ familyguy.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Family Guy</title>
+</head>
+<body>
+    <link rel="stylesheet" href="CSS/family-guy.css">
+    <ul height="300" width="50">
+        <li1><a href="home.html">Home</a></li>
+        <li2><a href="Shows.html">Shows</a></li>
+        <li3><a href="Schedule.html">Schedule</a></li>
+        <li4><a href="About.html">About Us</a></li>
+      </ul>
+
+      <img src="https://ik.imagekit.io/goanimatenetwork/shipoopi">
+
+</body>
+</html>
